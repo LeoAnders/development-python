711ecac677185334a152497e62f19faec8821055
Leonardo Anders <leonardoanders15@gmail.com>
2023-05-02 21:34:16 -0300

    tabuada

---
+++ Exercices/while_for/tabuada.py
@@ -0,0 +1,6 @@
+
+for tabuada in range(1,11,1):
+    print(f"TABUADA DO {tabuada}: ")
+    for number in range(1, 11, 1):
+        sum = tabuada * number
+        print(f"{tabuada} x {number} = {sum}")
